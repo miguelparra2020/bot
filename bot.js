32dfdd11e0d50dd58ac8e5c87683381379da6ad0
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2021-06-24 15:51:13 -0500

    Updated

--- bot.js
+++ bot.js
@@ -14,3 +14,32 @@ bot.launch()
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
 // Enable graceful stop
+
+const { Telegraf } = require('telegraf')
+const fs = require('fs')
+require('dotenv')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+// TLS options
+const tlsOptions = {
+  key: fs.readFileSync('server-key.pem'),
+  cert: fs.readFileSync('server-cert.pem'),
+  ca: [
+    // This is necessary only if the client uses a self-signed certificate.
+    fs.readFileSync('client-cert.pem')
+  ]
+}
+
+// Set telegram webhook
+// The second argument is necessary only if the client uses a self-signed
+// certificate. Including it for a verified certificate may cause things to break.
+bot.telegram.setWebhook('https://server.tld:8443/secret-path', {
+  source: 'server-cert.pem'
+})
+
+// Start https webhook
+bot.startWebhook('/secret-path', tlsOptions, 8443)
+
+// Http webhook, for nginx/heroku users.
+bot.startWebhook('/secret-path', null, 5000)
